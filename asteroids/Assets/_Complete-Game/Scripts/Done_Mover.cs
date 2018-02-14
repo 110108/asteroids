b4219fb065c53459e0e325e24f44f70e40d9fc6f
110108 <31996737+110108@users.noreply.github.com>
2018-02-14 11:16:25 -0500

    Revert "messed with settings on shot velocity"
    This reverts commit 11ee573f784e2726fba3d3f92df2835dcaf5b949.

--- asteroids/Assets/_Complete-Game/Scripts/Done_Mover.cs
+++ asteroids/Assets/_Complete-Game/Scripts/Done_Mover.cs
@@ -5,14 +5,8 @@ public class Done_Mover : MonoBehaviour
 {
 	public float speed;
 
-	private Rigidbody rb;
-
-	void start ()
+	void Start ()
 	{
-		rb=(GetComponent<Rigidbody>());
-//		rb.rotation = Quaternion.identity;
-//		rb.velocity = (rb.rotation) * speed;
-	//	Vector3 v3 = new Vector3(Input.GetAxis("Horizontal"),0.0f,Input.GetAxis("Vertical"));
-		rb.velocity=(speed * (transform.TransformDirection(Vector3.forward)));
+		GetComponent<Rigidbody>().velocity = transform.forward * speed;
 	}
 }
